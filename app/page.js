b1ecf404f7995e105a4179e6a6ac376b64595110
jeromenatio <natio.jerome@gmail.com>
2023-11-09 09:36:54 +0400

    commit

--- app/page.js
+++ app/page.js
@@ -1,7 +1,47 @@
+"use client"
+
+import { Button } from "@/components/ui/button"
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { useState } from "react"
+
+
+
 export default function Home() {
+  const [files, setFiles] = useState();
+
+  const onSubmit = (e)=>{
+    e.preventDefault();
+    if(files.length && files.length > 0){
+      console.log(files);
+    }
+  }
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>TEST NEXT JS</h1>
+    <main>
+      <section className="text-gray-600 body-font">
+        <div className="container px-5 py-24 mx-auto">
+          <div className="flex flex-wrap -m-4">
+            <div className="lg:w-1/2 md:w-1/2 p-4 w-full">
+              <Card className="shadow-md">
+                <CardHeader>
+                  <CardTitle className="text-lg">Mes fichiers</CardTitle>
+                  <CardDescription className="text-xs">Gérer mes fichiers</CardDescription>                  
+                    <form onSubmit={onSubmit}>
+                      <div className="flex w-full max-w-sm items-center space-x-2">
+                          <Input type="file" placeholder="Choisir fichier(s)" multiple className="text-xs h-8 py-1 px-2"  onChange={(e)=>{e.target.files && setFiles(e.target.files)}}/>
+                          <Button type="submit" className="text-xs h-8 py-1 px-2">Envoyer</Button>
+                      </div>
+                    </form>
+                </CardHeader>
+                <CardContent>
+                </CardContent>
+                <CardFooter>
+                </CardFooter>
+              </Card>
+            </div>
+          </div>
+        </div>
+      </section>
     </main>
   )
 }
